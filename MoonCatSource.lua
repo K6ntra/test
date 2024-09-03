66736c2301de8bd169d918c6a660b8f5f3d48208
K6ntra <130297045+K6ntra@users.noreply.github.com>
2024-09-03 10:43:38 -0300

    Update MoonCatSource.lua

--- MoonCatSource.lua
+++ MoonCatSource.lua
@@ -21,7 +21,7 @@ local mooncatlib = {
       ["Color Theme"] = Color3.fromRGB(251, 251, 251),
       ["Color Text"] = Color3.fromRGB(243, 243, 243),
       ["Color Dark Text"] = Color3.fromRGB(180, 180, 180),
-      ["MoonCat Icon"] = "rbxassetid://102148214469417"
+      ["MoonCat Icon"] = "rbxassetid://129272509964324"
     }
   },
   Info = {
